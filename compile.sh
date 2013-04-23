3e319b0a8c459081d466190bcfce8ba9360cc5f1
Suman Jandhyala <sumanvyj@gmail.com>
2013-04-23 00:13:33 -0700

    Fix jline issues

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac -cp ./jars/plume.jar lib/edu/washington/cs/cse490h/lib/*.java jline-1.0.jar proj/*.java
+javac -cp './jars/plume.jar:./jars/jline.jar' lib/edu/washington/cs/cse490h/lib/*.java proj/*.java
 cd lib
 jar cvf ../jars/lib.jar edu/washington/cs/cse490h/lib/*.class
 
