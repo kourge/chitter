4b6922292d1843328e1e32e933e1af25a8bbd62e
Suman Jandhyala <sumanvyj@gmail.com>
2013-04-22 22:40:29 -0700

    Add ChitterConsole stub file and dependencies

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac -cp ./jars/plume.jar lib/edu/washington/cs/cse490h/lib/*.java proj/*.java
+javac -cp ./jars/plume.jar lib/edu/washington/cs/cse490h/lib/*.java jline-1.0.jar proj/*.java
 cd lib
 jar cvf ../jars/lib.jar edu/washington/cs/cse490h/lib/*.class
 
