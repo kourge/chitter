5445659e886cf7b95c8b1558a0e0b916c95d16d0
Wilson Lee <kourge@gmail.com>
2013-04-23 19:55:57 -0700

    Output a slightly more readable serialization format for Chits

--- proj/Chit.java
+++ proj/Chit.java
@@ -17,4 +17,23 @@ public class Chit implements Serializable {
 
     public String getText() { return this.text; }
     public long getTimestamp() { return this.timestamp; }
+
+    public String toString() {
+        return String.format("<Chit time=%d text=\"%s\">", this.timestamp, this.text);
+    }
+
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.writeUTF(String.format("%d\t%s", this.timestamp, this.text));
+    }
+
+    private void readObject(ObjectInputStream ois)
+    throws IOException, ClassNotFoundException {
+        String line = ois.readUTF();
+        int delimiter = line.indexOf("\t");
+
+        this.timestamp = Long.parseLong(line.substring(0, delimiter));
+        this.text = line.substring(delimiter + 1);
+    }
+
+    private void readObjectNoData() throws ObjectStreamException {}
 }
