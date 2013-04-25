91649a0df746ea0f1657a4910dd07a57c15baaa0
Riley Adams <rileya@cs.washington.edu>
2013-04-25 03:57:51 -0700

    Remove verbose from compile.sh

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 javac -Xlint:unchecked -cp './jars/plume.jar:./jars/jline.jar' lib/edu/washington/cs/cse490h/lib/*.java proj/*.java
 cd lib
-jar cvf ../jars/lib.jar edu/washington/cs/cse490h/lib/*.class
+jar cf ../jars/lib.jar edu/washington/cs/cse490h/lib/*.class
 
 exit
