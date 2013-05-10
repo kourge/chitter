6e668b31b88712735dd18fc8a9680658312cce71
Wilson Lee <kourge@gmail.com>
2013-05-10 00:33:34 -0700

    Provide a REPL for Jythonic experiments

---
+++ proj/Repl.java
@@ -0,0 +1,22 @@
+import org.python.core.*;
+import org.python.util.*;
+
+public class Repl {
+    private static final String PYTHON_HOME = "python.home";
+
+    public static void main(String[] args) {
+        if (System.getProperty(PYTHON_HOME) == null) {
+            System.setProperty(PYTHON_HOME, "");
+        }
+
+        PySystemState sys = Py.getSystemState();
+        sys.path.append(new PyString("proj/"));
+        sys.path.append(new PyString("lib/edu/washington/cs/cse490h/lib/"));
+
+        InteractiveConsole.initialize(
+            System.getProperties(), null, new String[0]
+        );
+        InteractiveConsole c = new InteractiveConsole();
+        c.interact();
+    }
+}
