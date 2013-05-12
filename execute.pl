a0d2d199d67eec1879a85daecbcf06b1a78a3553
Suman Jandhyala <sumanvyj@gmail.com>
2013-05-12 13:47:49 -0700

    Go back to perl scripts

---
+++ execute.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+# Simple script to start a Node Manager that uses a compiled lib.jar
+
+main();
+
+sub main {
+
+    $classpath = "proj/:jars/plume.jar:jars/lib.jar:jars/jline.jar:jars/jython.jar:jars/plyjy.jar";
+
+    $args = join " ", @ARGV;
+
+    exec("java -cp $classpath edu.washington.cs.cse490h.lib.MessageLayer $args");
+}
