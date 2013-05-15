0ba77855d4cc0cf7d00ac0a8b5b2ebacee3b341b
Wilson Lee <kourge@gmail.com>
2013-05-15 14:37:51 -0700

    Only return a byte array from TransactionalFS::read(String)

--- proj/TransactionalFS.java
+++ proj/TransactionalFS.java
@@ -5,7 +5,7 @@
 public interface TransactionalFS {
     public long create(String filename);
     public boolean exists(String filename);
-    public Pair<byte[], Long> read(String filename);
+    public byte[] read(String filename);
     public long currentVersion(String filename);
     public long append(String filename, byte[] data);
     public long overwrite(String filename, byte[] data);
