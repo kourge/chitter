24ef7008ce722e91e87a51671f3ae6afa893a190
Riley Adams <rileya@cs.washington.edu>
2013-06-04 12:59:44 -0700

    Add client and server operation journals.

---
+++ proj/ClientJournal.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import edu.washington.cs.cse490h.lib.*;
+
+/** Journal used to log and complete client operations */
+public class ClientJournal {
+    
+    public ClientJournal(String filename, Node n) {
+        super(filename, n);
+    }
+
+    public void execute() {
+        // TODO fill this in when the new transaction semantics are figured
+    }
+}
---
+++ proj/ServerJournal.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import edu.washington.cs.cse490h.lib.*;
+
+/** Journal used to log and complete server operations, this will only store completed
+ *  transaction IDs, basically we use this to recover a Set of transactions the server
+ *  has completed (if a client resends a transaction ID we already finished, then we
+ *  can just tell them we're already done). This is a bit of a stretch as far as
+ *  extending 'Journal' since it's behavior is fairly distinct from other subclasses,
+ *  but it is still convenient to reuse some code here. */
+public class ServerJournal {
+    public ServerJournal(String filename, Node n) {
+        super(filename, n);
+    }
+
+    public void execute() {
+        // TODO
+    }
+}
