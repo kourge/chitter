7def1cd9fcb32e710cf643cbbb5743c69a481d4d
Wilson Lee <kourge@gmail.com>
2013-06-07 10:12:21 -0700

    Improve PaxosMessage pretty printing

--- proj/paxos_message.py
+++ proj/paxos_message.py
@@ -20,13 +20,11 @@ class PaxosMessage:
         self.data = value
 
     def __repr__(self):
-        return "%s(kind=%r, data=%r)" % (
-            self.__class__.__name__, self.kind, self.data
-        )
+        return "%s%r" % (self.kind, self.data)
 
 # Map each kind to a function that constructs a corresponding PaxosMessage.
 for kind in TYPES:
-    globals()[kind] = (lambda name: lambda *args: PaxosMessage(name, args))(kind.lower())
+    globals()[kind] = (lambda name: lambda *args: PaxosMessage(name, args))(kind)
 
 __all__ = ['PaxosMessage']
 __all__.extend(TYPES)
